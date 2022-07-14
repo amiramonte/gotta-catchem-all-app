4d0998dfdb7767439e0b068b43e1ef546d309960
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-07-14 14:52:54 -0700

    imported React on app.js file, imported CssBaseline on app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import { CssBaseline } from "@mui/material";
+
 
 function App() {
   return (
     <div>
+      <CssBaseline/>
       <h1>Welcome React Test</h1>
     </div>
   );
