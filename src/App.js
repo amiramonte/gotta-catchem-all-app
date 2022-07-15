af728c9d3d21a3ce55ba35f03b9ae38023d6324a
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-07-15 12:27:52 -0700

    added npm package for material UI icons, added navbar.js file under components folder, added MUI code for nav bar, imported navbar.js file to app.js file and input nav bar into react program, confirmed render of nav bar in dev environment on local machine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
+import ButtonAppBar from "./components/navbar";
 
 
 function App() {
   return (
     <div>
       <CssBaseline/>
+      <ButtonAppBar />
       <h1>Welcome React Test</h1>
     </div>
   );
