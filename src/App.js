e19ca175874116ac14bf24f0a129b5b149c0104c
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-07-21 16:38:41 -0700

    fixed counter component import on app.js file and counter.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
 import ButtonAppBar from "./components/navbar";
+import Counter from './components/counter';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <CssBaseline/>
       <ButtonAppBar />
       <h1>Welcome React Test</h1>
+      <Counter/>
     </div>
   );
 }
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,19 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 
-export default function counter() {
-
-    const [count, setCount] = useState(0);
-
-
-
+export default function Counter() {
 
     return (
     <div>
         <Box sx={{ width: '100%' }}>
             <Stack spacing={2}>
-                <p>Testing</p>
+                <p>testing</p>
             </Stack>
         </Box>
     </div>
