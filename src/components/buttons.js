ac7654c4205944262bdf5d39a177a2545aca35dc
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-07-21 16:46:37 -0700

    imported Stack and Button from MUI for buttons.js file, created stack of clickable buttons for adding, subtracting, and resetting the throw counter, imported Buttons into counter.js file and added to exported default function, tested everything in development environment and everything is successfully showing up

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 
 export default function Buttons() {
     return (
-        <div>Buttons</div>
+        <div>
+            <Stack spacing = {2}>
+                <Button variant="contained">+</Button>
+                <Button variant="contained">-</Button>
+                <Button variant="contained">reset</Button>
+            </Stack>
+        </div>
     )
 }
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
+import Buttons from './buttons';
 
 export default function Counter() {
 
@@ -9,6 +10,7 @@ export default function Counter() {
         <Box sx={{ width: '100%' }}>
             <Stack spacing={2}>
                 <p>testing</p>
+                <Buttons/>
             </Stack>
         </Box>
     </div>
