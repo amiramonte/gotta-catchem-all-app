6c16b70f93bd7f6e50fc7a8354e8d7f826d5e4a6
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-07-21 16:30:12 -0700

    imported Box and Stack from MUI, set up box with stack and added p tag for test of front end render

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,21 @@
 import React, {useState} from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 export default function counter() {
 
     const [count, setCount] = useState(0);
-    
+
 
 
 
     return (
-    <div>counter</div>
+    <div>
+        <Box sx={{ width: '100%' }}>
+            <Stack spacing={2}>
+                <p>Testing</p>
+            </Stack>
+        </Box>
+    </div>
     )
 }
